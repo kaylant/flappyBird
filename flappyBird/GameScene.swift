b98986e739fa16eb57046dee3f9a93cbe93d6292
kaylant <kaylant@gmail.com>
2016-09-10 17:43:55 -0500

    initial setup

---
+++ flappyBird/GameScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameScene.swift
+//  flappyBird
+//
+//  Created by Kaylan Smith on 9/10/16.
+//  Copyright (c) 2016 Kaylan Smith. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+        
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
