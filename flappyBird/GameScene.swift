ead086fd232766e513d4540ce28611d7ddd42b0e
kaylant <kaylant@gmail.com>
2016-09-11 18:01:11 -0500

    added animation

--- flappyBird/GameScene.swift
+++ flappyBird/GameScene.swift
@@ -9,9 +9,31 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
 
+        // image you use to display a certain sprite is referred to as a texture
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        // to animate:
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+        // animate between bird texture 1 and 2
+        let animation = SKAction.animateWithTextures([birdTexture, birdTexture2], timePerFrame: 0.1)
+        // to repeat the animation forever
+        let makeBirdFlap = SKAction.repeatActionForever(animation)
         
+        // apply it to bird
+        bird = SKSpriteNode(texture: birdTexture)
+        
+        // give it a location in the middle of the screen
+        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        // apply the animation to the bird node
+        bird.runAction(makeBirdFlap)
+        
+        // add to scene (screen)
+        self.addChild(bird)
         
     }
     
